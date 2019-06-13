148a8848c9a41c3a7ec9494e8f8aeaa12a723256
PenRain <15535702013@163.com>
2019-06-13 10:28:05 +0800

    lll

---
+++ src/main/java/com/demo/text.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+/**
+ * @version V1.0
+ * @title: text
+ * @projecName: demo_git
+ * @ClassName: com.demo.text.java
+ * @description: TODO
+ * @Copyright: 2019-2023 -Never bend
+ * @author: Admin
+ * @date: 2019/6/13 10:21
+ */
+public class text {
+    public static void main(String[] args) {
+        System.out.println("llll");
+    }
+}
