bd438ff37e150503b0a795e068309fcc2b8d8250
PenRain <15535702013@163.com>
2019-06-13 09:54:19 +0800

    hello git

---
+++ src/main/java/com/jiyun/GitTest.java
@@ -0,0 +1,18 @@
+package com.jiyun;
+
+/**
+ * @version V1.0
+ * @title: GitTest
+ * @projecName: git_demo
+ * @ClassName: com.jiyun.GitTest.java
+ * @description: TODO
+ * @Copyright: 2019-2023 -Never bend
+ * @author: Admin
+ * @date: 2019/6/13 9:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("lalallalal");
+    }
+
+}
